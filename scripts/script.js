e2e713b2a470bcbc648b9eaf0b9461f8f991151f
Vips43 <vipulgehlot43@gmail.com>
2025-07-12 23:07:24 +0530

    a

--- scripts/script.js
+++ scripts/script.js
@@ -1,52 +1,71 @@
-let taskValue = document.querySelector('#input')
-let ol = document.querySelector('.task')
+// let taskValue = document.querySelector('#input')
+// let ol = document.querySelector('.task')
 
 
-function addTask(){
-    if(taskValue.value !== ''){
-        work();
-        taskValue.value = ''
-        saveData();
-    }
-    else{
-        return
-    }
-}
+// function addTask(){
+//     if(taskValue.value !== ''){
+//         work();
+//         taskValue.value = ''
+//         saveData();
+//     }
+//     else{
+//         return
+//     }
+// }
 
-function work(){
-    let li = document.createElement('li')
-    let span = document.createElement('span')
+// function work(){
+//     let li = document.createElement('li')
+//     let span = document.createElement('span')
     
-    ol.appendChild(li)
-    li.appendChild(span)
-    span.innerText = taskValue.value
+//     ol.appendChild(li)
+//     li.appendChild(span)
+//     span.innerText = taskValue.value
     
-    deleteBtn(li, span);
-    saveData();
-}
-function deleteBtn(li, span) {
-    let button = document.createElement('button')
-    li.appendChild(button)
-    button.innerText = 'X';
-    saveData();
-    span.addEventListener('click',()=>{
-        span.classList.add('checked')
-        saveData();
-    })
-    button.addEventListener('click', ()=>{
-        button.parentElement.remove()
-        saveData();
-    })
+//     saveData();
+//     deleteBtn(li, span);
+// }
+// function deleteBtn(li, span) {
+//     let button = document.createElement('button')
+//     li.appendChild(button)
+//     button.innerText = 'X';
+//     saveData();
+//     span.addEventListener('click',()=>{
+//         saveData();
+//         span.classList.toggle('checked')
+//     })
+//     button.addEventListener('click', ()=>{
+//         saveData();
+//         button.parentElement.remove()
+//     })
+// }
+
+// function saveData() {
+
+//     localStorage.setItem('data', ol.innerHTML)
+    
+// }
+// function showData() {
+//     ol.innerHTML = localStorage.getItem('data')
+// }
+// showData();
+
+// function clea() {
+//     localStorage.clear()
+// }
+
+let inputTask = document.getElementById('input');
+let taskList = document.getElementsByClassName('task')
+
+let tasks = [];
+
+function addBtn(){
+    addTodo();
+
 }
 
-function saveData() {
-    localStorage.setItem('data', ol.innerHTML)
-}
-function showData() {
-    ol.innerHTML = localStorage.getItem('data')
-}
-showData();
-
-function clea() {
-    localStorage.clear()
+function addTodo(){
+    const task = inputTask.value.trim();
+    tasks.push(task);
+    console.log(tasks);
+    
 }
