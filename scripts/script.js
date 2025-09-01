91d65111f4f7f5e21ecb54c84e36fd0486daebf9
Vips43 <vipulgehlot43@gmail.com>
2025-09-01 13:37:29 +0530

    final product

--- scripts/script.js
+++ scripts/script.js
@@ -1,76 +1,88 @@
-// let taskValue = document.querySelector('#input')
-// let ol = document.querySelector('.task')
-
-
-// function addTask(){
-//     if(taskValue.value !== ''){
-//         work();
-//         taskValue.value = ''
-//         saveData();
-//     }
-//     else{
-//         return
-//     }
-// }
-
-// function work(){
-//     let li = document.createElement('li')
-//     let span = document.createElement('span')
-
-//     ol.appendChild(li)
-//     li.appendChild(span)
-//     span.innerText = taskValue.value
-
-//     saveData();
-//     deleteBtn(li, span);
-// }
-// function deleteBtn(li, span) {
-//     let button = document.createElement('button')
-//     li.appendChild(button)
-//     button.innerText = 'X';
-//     saveData();
-//     span.addEventListener('click',()=>{
-//         saveData();
-//         span.classList.toggle('checked')
-//     })
-//     button.addEventListener('click', ()=>{
-//         saveData();
-//         button.parentElement.remove()
-//     })
-// }
-
-// function saveData() {
-
-//     localStorage.setItem('data', ol.innerHTML)
-
-// }
-// function showData() {
-//     ol.innerHTML = localStorage.getItem('data')
-// }
-// showData();
-
-// function clea() {
-//     localStorage.clear()
-// }
-
 let input = document.querySelector('#input')
-let ol = document.querySelector('.task')
+let ol = document.querySelector('.task'),
+  addEditBtn = document.querySelector('.addBtn'),
+  warn = document.getElementById('warn')
 
-let allTodos = [];
+let todoSpan = null
 
 function addBtn() {
-    if(input.value == ''){
-        return
-    }else{
-        let todoText = input.value.trim()
-        allTodos.push(todoText)
-        createTodoItem(todoText);
-        ol.innerHTML = allTodos
-        input.value = '';
+  if (addEditBtn.innerText == 'Edit') {
+    const confirmation = confirm('Sure to Edit')
+    if (confirmation) {
+      todoSpan.innerText = input.value
+      addEditBtn.innerText = 'Add'
+      input.value = ''
+      showWarn("Edited successfully", "#c2a009")
+      saveTasks()
     }
+  } else {
+
+    let inputValue = input.value.trim()
+    if (inputValue === '') {
+      showWarn("Please Write something", "red")
+      return
+    } else {
+      addTask(inputValue)
+      input.value = ''
+      showWarn("Task added", "green")
+      saveTasks()
+    }
+  }
 }
-function createTodoItem(todo) {
-    const todoLI = document.createElement("li");
-    todoLI.innerText = todo;
-    ol.append(todoLI)
+
+function addTask(task) {
+  let li = document.createElement('li'),
+    span = document.createElement('span'),
+    editbtn = document.createElement('button'),
+    delbtn = document.createElement('button')
+
+  span.innerText = task
+  editbtn.innerText = 'Edit'
+  delbtn.innerText = 'Del'
+  editbtn.classList = 'editBtn'
+  delbtn.classList = 'deletBtn'
+
+  li.append(span, editbtn, delbtn);
+  ol.appendChild(li)
+
+  //delete tasks
+  delbtn.addEventListener('click', () => {
+    li.remove()
+    showWarn("Task Deleted", "black")
+    saveTasks()
+  })
+
+  // edit task
+  editbtn.addEventListener('click', () => {
+    input.value = span.innerText
+    input.focus()
+    addEditBtn.innerText = 'Edit'
+    todoSpan = span
+    console.log(todoSpan)
+    saveTasks()
+  })
+
 }
+function showWarn(warning,color) {
+  warn.classList.add('show')
+  warn.style.color = color
+  warn.innerText = warning
+  setTimeout(() => {
+    warn.classList.remove('show')
+  }, 1500);
+}
+
+function saveTasks() {
+  const tasks = []
+  document.querySelectorAll('.task li span').forEach(span=>{
+    tasks.push(span.innerText)
+  })
+  localStorage.setItem('tasks',JSON.stringify(tasks))
+}
+
+function loadTasks() {
+  const storedTasks = JSON.parse(localStorage.getItem('tasks')) || []
+  storedTasks.forEach(task => addTask(task))
+}
+
+window.addEventListener('DOMContentLoaded', loadTasks)
